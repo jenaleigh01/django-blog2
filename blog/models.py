8725df21d6e87d56e59dbf3ed44f1f07237c170f
Jenna Guest <jenaleigh01@gmail.com>
2024-05-06 00:12:12 +0000

    Added crud functionality to comments for users

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    Post = models.ForeignKey(
+    post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
